790e35ecafe95c1cb45a72cc17beb427ef6a873c
UniTaeMin <onnu0802@naver.com>
2020-04-19 03:21:39 +0900

    Delete Comment

--- src/Routers/index.ts
+++ src/Routers/index.ts
@@ -1,7 +1,5 @@
 import * as express from "express"
 import Post from "./post/post.router"
-import Comment from "./comment/comment.router"
 const router = express.Router()
 router.use("/post", Post)
-router.use("/comment", Comment)
 export default router
